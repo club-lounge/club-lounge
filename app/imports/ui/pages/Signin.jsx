73abf3805063bde1263531230f9b3c9c4b3870ec
JunM1ao <45115605+JunM1ao@users.noreply.github.com>
2020-04-10 12:07:30 -1000

    Update Signin.jsx
    updated sign in with uniforms

--- app/imports/ui/pages/Signin.jsx
+++ app/imports/ui/pages/Signin.jsx
@@ -1,33 +1,37 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Link, Redirect } from 'react-router-dom';
+import { Link, Redirect, NavLink } from 'react-router-dom';
+import { AutoForm, ErrorsField, SubmitField, TextField } from 'uniforms-semantic';
+import swal from 'sweetalert';
 import { Meteor } from 'meteor/meteor';
-import { Container, Form, Grid, Header, Message, Segment } from 'semantic-ui-react';
+import 'uniforms-bridge-simple-schema-2'; // required for Uniforms
+import SimpleSchema from 'simpl-schema';
+import { Container, Grid, Header, Message, Segment, Button } from 'semantic-ui-react';
+
+const formSchema = new SimpleSchema({
+  email: String,
+  password: String,
+});
 
 /**
  * Signin page overrides the form’s submit event and call Meteor’s loginWithPassword().
  * Authentication errors modify the component’s state to be displayed
  */
 export default class Signin extends React.Component {
-
-  /** Initialize component state with properties for login and redirection. */
   constructor(props) {
     super(props);
-    this.state = { email: '', password: '', error: '', redirectToReferer: false };
-  }
-
-  /** Update the form controls each time the user interacts with them. */
-  handleChange = (e, { name, value }) => {
-    this.setState({ [name]: value });
+    this.state = { error: '', redirectToReferer: false };
   }
 
   /** Handle Signin submission using Meteor's account mechanism. */
-  submit = () => {
-    const { email, password } = this.state;
+  submit(data, formRef) {
+    const { email, password } = data;
     Meteor.loginWithPassword(email, password, (err) => {
       if (err) {
         this.setState({ error: err.reason });
+        swal('Error', err.message, 'error');
       } else {
+        formRef.reset();
         this.setState({ error: '', redirectToReferer: true });
       }
     });
@@ -35,6 +39,7 @@ export default class Signin extends React.Component {
 
   /** Render the signin form. */
   render() {
+    let fRef = null;
     const { from } = this.props.location.state || { from: { pathname: '/' } };
     // if correct authentication, redirect to page instead of login screen
     if (this.state.redirectToReferer) {
@@ -43,46 +48,21 @@ export default class Signin extends React.Component {
     // Otherwise return the Login form.
     return (
       <Container style={{ marginTop: '2em' }}>
-        <Grid textAlign="center" verticalAlign="middle" centered columns={2}>
+        <Grid textAlign="center" verticalAlign="middle" centered>
           <Grid.Column>
-            <Header as="h2" textAlign="center" inverted>
-              Login to your account
-            </Header>
-            <Form onSubmit={this.submit}>
-              <Segment stacked>
-                <Form.Input
-                  label="Email"
-                  icon="user"
-                  iconPosition="left"
-                  name="email"
-                  type="email"
-                  placeholder="E-mail address"
-                  onChange={this.handleChange}
-                />
-                <Form.Input
-                  label="Password"
-                  icon="lock"
-                  iconPosition="left"
-                  name="password"
-                  placeholder="Password"
-                  type="password"
-                  onChange={this.handleChange}
-                />
-                <Form.Button content="Submit"/>
+            <Header as="h2" textAlign="center" inverted>Login to your account</Header>
+            <AutoForm ref={ref => { fRef = ref; }} schema={formSchema} onSubmit={data => this.submit(data, fRef)} >
+              <Segment>
+                <TextField name='email' placeholder='E-mail address' iconLeft='user'/>
+                <TextField name='password' type='password' placeholder='Password' iconLeft='lock'/>
+                <SubmitField value='Login'/>
+                <Button as={NavLink} floated='right' color='teal' exact to='/'>Back to Home Page</Button>
+                <ErrorsField/>
               </Segment>
-            </Form>
+            </AutoForm>
             <Message>
               <Link to="/signup">Click here to Register</Link>
             </Message>
-            {this.state.error === '' ? (
-              ''
-            ) : (
-              <Message
-                error
-                header="Login was not successful"
-                content={this.state.error}
-              />
-            )}
           </Grid.Column>
         </Grid>
       </Container>
