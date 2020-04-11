689db8e0a79d76eb10941636d7f622d7591f3a7b
Sang Jin Lee <sangjinlee111@gmail.com>
2020-04-10 22:08:43 -1000

    Figuring out search bar

--- app/imports/ui/pages/JoinClub.jsx
+++ app/imports/ui/pages/JoinClub.jsx
@@ -1,13 +1,42 @@
 import React from 'react';
+import _ from 'lodash';
 import { Meteor } from 'meteor/meteor';
-import { Container, Header, Loader, Card } from 'semantic-ui-react';
+import { Container, Header, Loader, Card, Search, Grid, Segment } from 'semantic-ui-react';
 import { withTracker } from 'meteor/react-meteor-data';
 import PropTypes from 'prop-types';
 import Club from '../components/Club';
 import { Clubs } from '../../api/club/Clubs';
 
+const source = _.times(5, () => ({
+  title: this.props.clubs.clubName,
+  description: this.props.clubs.description,
+  image: this.props.clubs.image,
+}));
+
+const initialState = { isLoading: false, results: [], value: '' };
+
 /** Renders a table containing all of the Stuff documents. Use <StuffItem> to render each row. */
 class JoinClub extends React.Component {
+  state = initialState
+
+  handleResultSelect = (e, { result }) => this.setState({ value: result.title })
+
+  handleSearchChange = (e, { value }) => {
+    this.setState({ isLoading: true, value });
+
+    // eslint-disable-next-line consistent-return
+    setTimeout(() => {
+      if (this.state.value.length < 1) return this.setState(initialState);
+
+      const re = new RegExp(_.escapeRegExp(this.state.value), 'i');
+      const isMatch = (result) => re.test(result.title);
+
+      this.setState({
+        isLoading: false,
+        results: _.filter(source, isMatch),
+      });
+    }, 300);
+  }
 
   /** If the subscription(s) have been received, render the page, otherwise show a loading icon. */
   render() {
@@ -16,6 +45,7 @@ class JoinClub extends React.Component {
 
   /** Render the page once subscriptions have been received. */
   renderPage() {
+    const { isLoading, value, results } = this.state;
     return (
         <Container>
           <Header as="h2" textAlign="center" inverted>Join a Club</Header>
@@ -24,6 +54,33 @@ class JoinClub extends React.Component {
                 key={index}
                 club={club}/>)}
           </Card.Group>
+          <Grid>
+            <Grid.Column width={6}>
+              <Search
+                  fluid
+                  loading={isLoading}
+                  onResultSelect={this.handleResultSelect}
+                  onSearchChange={_.debounce(this.handleSearchChange, 500, {
+                    leading: true,
+                  })}
+                  results={results}
+                  value={value}
+                  {...this.props}
+              />
+            </Grid.Column>
+            <Grid.Column width={10}>
+              <Segment>
+                <Header>State</Header>
+                <pre style={{ overflowX: 'auto' }}>
+              {JSON.stringify(this.state, null, 2)}
+            </pre>
+                <Header>Options</Header>
+                <pre style={{ overflowX: 'auto' }}>
+              {JSON.stringify(source, null, 2)}
+            </pre>
+              </Segment>
+            </Grid.Column>
+          </Grid>
         </Container>
     );
   }
