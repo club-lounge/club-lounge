c53888646cdd7a498d952d2d3e0bf6a672888f2c
vrabang <boque.rabang@gmail.com>
2020-04-24 01:13:40 -1000

    Update main.js

--- app/server/main.js
+++ app/server/main.js
@@ -3,14 +3,22 @@ import '/imports/startup/server/Publications';
 import '/imports/startup/server/Mongo';
 import { Email } from 'meteor/email';
 
-process.env.MAIL_URL = 'smtps://uhclublounge%40gmail.com:clublounge2020@smtp.gmail.com:465/';
+Meteor.methods({
+  // eslint-disable-next-line meteor/audit-argument-checks
+  registerEmail: function (email, firstName) {
+    process.env.MAIL_URL = 'smtps://uhclublounge%40gmail.com:clublounge2020@smtp.gmail.com:465/';
 
-let message = 'Hello ,<br>';
-message += 'Thank you for registering in the Club Lounge website, hope you find all the future clubs and events ' +
-    'enjoyable';
-Email.send({
-  to: 'boque.rabang@gmail.com',
-  from: 'uhclublounge@gmail.com',
-  subject: 'Registration Confirmation',
-  text: message,
+    let message = `Hello ${firstName},<br>`;
+    message += 'Thank you for registering in the Club Lounge website, hope you find all the future clubs and events ' +
+        'enjoyable';
+    message += 'To access your account you can click' +
+        '<a href=\'http://clublounge.meteorapp.com/#/signin\'> this link</a>.<br>';
+    this.unblock();
+    Email.send({
+      to: email,
+      from: 'clublounge@gmail.com',
+      subject: 'Registration Confirmation',
+      text: message,
+    });
+  },
 });
